1f72a954d59245e4d95599986f8b227e0bb4a9a8
MykhailoForge <mike.k@forge.solutions>
2023-06-08 14:55:19 +0300

    FS-005: add cypress tests.

--- src/modules/language-selector/LanguageSelector.tsx
+++ src/modules/language-selector/LanguageSelector.tsx
@@ -15,12 +15,13 @@ export const LanguageSelector = ({ changeHandler }: LanguageSelectorProps) => {
   const selectedLanguage = useAppSelector(selectedLanguageSelector);
 
   return (
-    <Box sx={{ maxWidth: 120 }}>
+    <Box sx={{ maxWidth: 120 }} data-testid="language-selector-container">
       <TextField
         select
         onChange={changeHandler}
         fullWidth
         value={selectedLanguage}
+        data-testid="language-selector-body"
       >
         {languageArr.map((langItem) => (
           <MenuItem value={langItem.item} key={langItem.id}>
