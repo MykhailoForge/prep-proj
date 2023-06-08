d992eb9926383bc50e361045e08279f3e104765e
MykhailoForge <mike.k@forge.solutions>
2023-06-08 14:55:19 +0300

    FS-005: add cypress tests.

---
+++ cypress/e2e/QueueVisualizer.cy.ts
@@ -0,0 +1,59 @@
+import uaLocalization from "../../src/assets/i18n/ua.json";
+
+const QUEUE_TEST_MSG = "Test item";
+const PATH_NAME_QUEUE = "/queue";
+const QUEUE_LANG_MATCHER =
+  uaLocalization.translation.queueVisualizer.buttons.add;
+
+const inputTextAddNewQueueItem = () => {
+  cy.get(`[data-testid="data-list-text-input"]`).type(QUEUE_TEST_MSG);
+  cy.get(`[data-testid="data-list-add-button"]`).click();
+  cy.get(`[data-testid="data-list-items-container"]`)
+    .children()
+    .should("have.length", 4);
+  cy.contains(QUEUE_TEST_MSG);
+};
+
+const removeQueueItem = () => {
+  cy.get(`[data-testid="data-list-remove-button"]`).click();
+  cy.get(`[data-testid="data-list-items-container"]`)
+    .children()
+    .should("have.length", 2);
+};
+
+const processQueueBackButton = () => {
+  cy.get(`[data-testid="data-list-back-button"]`).click();
+  cy.location("pathname").should("eq", "/");
+};
+
+const processQueueSelectLanguage = () => {
+  cy.get(`[data-testid="language-selector-body"]`).click();
+  cy.get(".css-1ka5eyc-MuiPaper-root-MuiMenu-paper-MuiPopover-paper")
+    .children()
+    .get("ul li")
+    .last()
+    .click();
+  cy.get(`[data-testid="data-list-add-button"]`).contains(QUEUE_LANG_MATCHER);
+};
+
+describe(`Testing "${PATH_NAME_QUEUE}" route`, () => {
+  beforeEach(() => {
+    cy.visit(PATH_NAME_QUEUE);
+  });
+
+  it("Should add new list item", () => {
+    expect(inputTextAddNewQueueItem());
+  });
+
+  it("Should remove last item from list", () => {
+    expect(removeQueueItem());
+  });
+
+  it(`Should proceed to "/" route`, () => {
+    expect(processQueueBackButton());
+  });
+
+  it("Should change language", () => {
+    expect(processQueueSelectLanguage());
+  });
+});
