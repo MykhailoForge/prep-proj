687405adb8af6f4d8feb4f53bf0fdf213751a05c
MykhailoForge <mike.k@forge.solutions>
2023-06-16 13:35:30 +0300

    FS-009: fix vietst alias

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { resolve, dirname } from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,12 +11,12 @@ export default defineConfig({
     host: true,
     port: 5173,
   },
+  resolve: { alias: [{ find: "@", replacement: resolve(__dirname, "./src") }] },
   test: {
     environment: "jsdom",
     setupFiles: "./src/__tests__/setupTests.ts",
     globals: true,
-    alias: {
-      "@/": new URL("./src/", import.meta.url).pathname,
-    },
+
+    alias: [{ find: "@", replacement: resolve(__dirname, "./src") }],
   },
 });
